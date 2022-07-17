77ef5af1b7083b8d46c0322437f27e43bd8f8ea8
colorlessenergy <brianmunozdev@gmail.com>
2022-07-16 18:45:58 -0700

    add link to deck

--- components/DisplayDecks.js
+++ components/DisplayDecks.js
@@ -38,7 +38,7 @@ const DisplayDecks = () => {
                                         <path d="M7 6V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5zm2-2v2h6V4H9z" />
                                     </svg>
                                 </button>
-                                <button>
+                                <a href={deck.link}>
                                     <svg
                                         className="icon"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +47,7 @@ const DisplayDecks = () => {
                                         height="24">
                                         <path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2z" />
                                     </svg>
-                                </button>
+                                </a>
                             </div>
                         </div>
                         <div className="deck-images-container">
