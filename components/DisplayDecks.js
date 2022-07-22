06c0e6b4b17fa721c364499601a0b72a1ccbd752
colorlessenergy <brianmunozdev@gmail.com>
2022-07-22 14:41:04 -0700

    add titles to deck actions

--- components/DisplayDecks.js
+++ components/DisplayDecks.js
@@ -64,6 +64,7 @@ const DisplayDecks = () => {
                                         onClick={() =>
                                             handleDeleteDeck(deck.ID)
                                         }
+                                        title="delete deck"
                                         className="mr-1">
                                         <svg
                                             className="icon"
@@ -74,7 +75,7 @@ const DisplayDecks = () => {
                                             <path d="M7 6V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5zm2-2v2h6V4H9z" />
                                         </svg>
                                     </button>
-                                    <a href={deck.link}>
+                                    <a href={deck.link} title="copy deck">
                                         <svg
                                             className="icon"
                                             xmlns="http://www.w3.org/2000/svg"
