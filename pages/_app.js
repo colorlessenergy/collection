cb477f57d2748408ef3a199e95d92eee125b1a5a
colorlessenergy <brianmunozdev@gmail.com>
2022-08-24 12:06:47 -0700

    move decks initialization to display decks

--- pages/_app.js
+++ pages/_app.js
@@ -3,42 +3,6 @@ import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
-        if (!localStorage.getItem('decks')) {
-            localStorage.setItem(
-                'decks',
-                JSON.stringify([
-                    {
-                        ID: 1,
-                        link: 'https://link.clashroyale.com/deck/en?deck=27000000;28000000;26000021;26000038;26000030;26000014;26000010;28000011',
-                        cards: [
-                            '27000000',
-                            '28000000',
-                            '26000021',
-                            '26000038',
-                            '26000030',
-                            '26000014',
-                            '26000010',
-                            '28000011'
-                        ]
-                    },
-                    {
-                        ID: 2,
-                        link: 'https://link.clashroyale.com/deck/en?deck=27000000;26000040;28000004;26000030;26000065;28000003;26000019;28000011',
-                        cards: [
-                            '27000000',
-                            '26000040',
-                            '28000004',
-                            '26000030',
-                            '26000065',
-                            '28000003',
-                            '26000019',
-                            '28000011'
-                        ]
-                    }
-                ])
-            );
-        }
-
         if (!localStorage.getItem('ID')) {
             localStorage.setItem('ID', JSON.stringify(2));
         }
