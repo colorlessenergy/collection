d1525e54f20428b6183df5c4e113bc834d4f4c88
colorlessenergy <brianmunozdev@gmail.com>
2023-01-12 12:21:06 -0800

    update images and manifest.json

--- pages/_document.js
+++ pages/_document.js
@@ -21,13 +21,10 @@ class MyDocument extends Document {
                     />
                     <meta name="format-detection" content="telephone=no" />
                     <meta name="mobile-web-app-capable" content="yes" />
-                    <meta name="msapplication-TileColor" content="#333333" />
+                    <meta name="msapplication-TileColor" content="#222222" />
                     <meta name="msapplication-tap-highlight" content="no" />
-                    <meta name="theme-color" content="#333333" />
-                    <link
-                        rel="apple-touch-icon"
-                        href="/icons/apple-touch-icon.png"
-                    />
+                    <meta name="theme-color" content="#222222" />
+                    <link rel="apple-touch-icon" href="/icons/192.png" />
 
                     <link rel="manifest" href="/manifest.json" />
                 </Head>
